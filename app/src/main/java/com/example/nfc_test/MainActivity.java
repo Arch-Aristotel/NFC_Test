a16bb0cbce66aa1614a743ac0bf4b78709eb9316
Arch-Aristotel <9150939900@mail.ru>
2023-02-13 01:00:12 +0300

    13.02.23

--- app/src/main/java/com/example/nfc_test/MainActivity.java
+++ app/src/main/java/com/example/nfc_test/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.nfc_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private NfcAdapter mNFC;
+    String a;
 
     TextView txt;
     @Override
@@ -22,20 +25,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = findViewById(R.id.textView);
-        mNFC = (NfcAdapter.getDefaultAdapter(this));
+
 
 
 
     }
     public void get_Start(View v){
+        mNFC = (NfcAdapter.getDefaultAdapter(this));
         if(!mNFC.isEnabled())
         txt.setText("NFC Disabled");
         else{
             txt.setText("is good");
-
+            NFC_Adapt(getIntent());
+            txt.setText("Hello "+a);
 
         }
 
+    }
+
+    public void NFC_Adapt(Intent e){
+        String word = e.getAction();
+        if (mNFC.ACTION_TAG_DISCOVERED.equals(word) ||
+                mNFC.ACTION_TECH_DISCOVERED.equals(word) ||
+                    mNFC.ACTION_NDEF_DISCOVERED.equals(word)){
+            Tag t = e.getParcelableExtra(mNFC.EXTRA_TAG);
+            for(String w : t.getTechList()){
+                w=a;
+            }
+        }
+        else a="word";
     }
 
 
@@ -46,11 +64,12 @@ public class MainActivity extends AppCompatActivity {
         String type = i.getType();
         String action = i.getAction();
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)){
+            Parcelable[] parcs = i.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
 
-
+            for (Parcelable p : parcs) {
+                txt.setText(p.toString());
+                }
         }
     }
-
-
 
 }
